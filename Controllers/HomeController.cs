1e79f86a201e4a69404aec78d3b3f77b7978d413
vinicius-alves-santos <hancokmatade@gmail.com>
2019-12-07 12:03:39 -0300

    Criação

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,8 +12,7 @@ namespace Identity.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-          
-
+         
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
