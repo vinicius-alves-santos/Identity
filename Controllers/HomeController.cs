c75e9a0f07ef84362efb25b7bae41f0d2e1875ff
vinicius-alves-santos <hancokmatade@gmail.com>
2019-12-07 12:02:15 -0300

    Criação da Branch

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace Identity.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+          
 
         public HomeController(ILogger<HomeController> logger)
         {
